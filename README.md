52f5d0b60369a6fa74a07024cc13c24193156424
Kristen Joie <kjoie.dev@gmail.com>
2022-06-14 16:06:22 +0200

    Implement Twitter API

--- README.md
+++ README.md
@@ -6,10 +6,15 @@ This app will choose randomly from your favorites photos minus photos already in
 The downloaded photo will be file `'photo.jpg'`.
 
 ## 🚥 Pre-requisites
+### For Google Photo
 - Create Oauth Client ID and an API KEY
 You must on [Google Cloud Console](https://console.cloud.google.com) create an OAuth 2.0 Client ID and an API_KEY.  
 You can follow this tutorial --> https://gilesknap.github.io/gphotos-sync/main/tutorials/oauth2.html#client-id
 - Set Environment variable GOOGLE_CLIENT_SECRET and GOOGLE_CLIENT_ID, GOOGLE_API_KEY.
+
+### For Twitter
+- Set Environment variable TWITTER_API_KEY, TWITTER_API_KEY_SECRET, TWITTER_ACCESS_TOKEN and TWITTER_ACCESS_SECRET.  
+Follow this doc to generate token --> https://developer.twitter.com/en/docs/twitter-api/getting-started/getting-access-to-the-twitter-api
 
 ## 🏗️ Install
 Install pip packages:
@@ -19,6 +24,7 @@ pip3 install -r requirements.txt
 
 ## 🚀 Run
 
+### For Google Photo **Only**
 ```
 python3 download_gphoto.py
 ```
@@ -27,3 +33,11 @@ At the first launch, a new browser windows will be displayed on the authentifcat
 - Accept these Google Authorization.
 - In the console, the message `Your refresh token is: "..."`, add this to the environment variable GOOGLE_REFRESH_TOKEN
 
+### For Twitter **Only**
+```
+python3 twitter.py "My tweet text" <username>
+```
+This will send a tweet with the photo file `'photo.jpg'` and the text you choose. You can add an username of the twitter user you want to tag to the photo.
+
+## 🔗 Link
+Doc python package [tweepy](https://docs.tweepy.org/en/stable/index.html)
---
+++ twitter.py
@@ -0,0 +1,64 @@
+from tweepy import Client, API, OAuthHandler
+import os
+import sys
+
+class Twitter:
+    def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret):
+        self.client = Client(consumer_key=consumer_key, consumer_secret=consumer_secret, access_token=access_token, access_token_secret=access_token_secret)
+        
+        auth = OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        self.api = API(auth)
+
+    def get_current_user_id(self):
+        r = self.client.get_me()
+        return r.data.id 
+
+    def get_user_id(self, usename):
+        r = self.client.get_users(usernames=["kr1s1o"], user_auth=True)
+        return r.data[0].id
+
+    def get_user_last_tweet_date(self, user_id):
+        r = self.client.get_users_tweets(id=user_id, max_results=5, user_auth=True)
+        r = self.client.get_tweet(r.meta["newest_id"], tweet_fields=["created_at"], user_auth=True)
+        return r.data.created_at
+    
+    def tweet_photo(self, text, filepath, media_tagged_user_ids=None):
+        r = self.api.simple_upload(filepath)
+        media_id = r.media_id
+        media_tagged_user_ids = None if media_tagged_user_ids is None else [media_tagged_user_ids]
+        r = self.client.create_tweet(media_ids=[media_id], text=text, media_tagged_user_ids=media_tagged_user_ids)
+        return r
+
+if __name__ == "__main__":
+    consumer_key = os.getenv("TWITTER_API_KEY")
+    consumer_secret = os.getenv("TWITTER_API_KEY_SECRET")
+    access_token = os.getenv("TWITTER_ACCESS_TOKEN")
+    access_token_secret = os.getenv("TWITTER_ACCESS_SECRET")
+
+    if consumer_key is None or consumer_key == "" \
+        or consumer_secret is None or consumer_secret == "" \
+        or access_token is None or access_token == "" \
+        or access_token_secret is None or access_token_secret == "" :
+        print("Error Missing Environment Variable TWITTER_API_KEY, TWITTER_API_KEY_SECRET, TWITTER_ACCESS_TOKEN or TWITTER_ACCESS_SECRET")
+        exit(1)
+
+    if len(sys.argv) < 2:
+        print('You must add to the command the tweet text, you can add an user to tag.\nFor example: python3 twitter.py "My tweet text" friend_tag')
+        exit(1)
+
+    twitter = Twitter(consumer_key, consumer_secret, access_token, access_token_secret)
+
+    current_id = twitter.get_current_user_id()
+    last_tweet_date = twitter.get_user_last_tweet_date(current_id)
+    print("last tweet date : {}".format(last_tweet_date))
+
+    # upload  part
+    tagged_user = None
+    if len(sys.argv) > 2:
+        tagged_user = twitter.get_user_id(sys.argv[2])
+    if os.path.exists('photo.jpg'):
+        twitter.tweet_photo(sys.argv[1], 'photo.jpg', tagged_user)
+    else :
+        print('File "photo.jpg" does not exits')
+        exit(1)
